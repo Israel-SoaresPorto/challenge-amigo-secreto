bcb371cba8058ec5ef05fb6fc47f831392f9a662
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-02-20 16:56:19 -0300

    implementanto validação para adição de amigos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const addFriendForm = document.querySelector("#add-friend-form");
+const drawFriendButton = document.querySelector("#draw-friend-btn");
+const friendList = document.querySelector("#friends-list");
+const friendDraw = document.querySelector("#result-draw");
+const friendInput = document.querySelector("#friend-input");
+const errorMessage = document.querySelector("#error-message");
+
+const friends = [];
+
+addFriendForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!friendInput.checkValidity()) {
+    errorMessage.style.display = "block";
+    const validityState = friendInput.validity;
+
+    if (validityState.valueMissing) {
+      errorMessage.textContent = "O nome do amigo não pode estar vazio";
+    } else if (friends.includes(friendInput.value)) {
+      errorMessage.textContent = "Esse amigo já foi adicionado";
+    } else if (validityState.tooShort) {
+      errorMessage.textContent = "Digite um nome com mais de 3 caracteres";
+    }
+
+    setTimeout(() => {
+      errorMessage.style.display = "none";
+    }, 3000);
+
+    return;
+  }
+
+  friends.push(friendInput.value);
+  friendList.innerHTML += `<li>${friendInput.value}</li>`;
+  addFriendForm.reset();
+  errorMessage.style.display = "none";
+});
